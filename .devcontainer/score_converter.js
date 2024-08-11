0e810e1f69fa61cf05720a98868d93b6fbc1d2a2
Anshuman Majumder <65849258+ANSHUMANMAJUMDER@users.noreply.github.com>
2024-08-11 10:35:17 +0000

    studying number conversion

---
+++ .devcontainer/score_converter.js
@@ -0,0 +1,8 @@
+let score = "fgfgfgfg";
+let ans = typeof(score);
+console.log(ans);
+ //changing the value of the number is happening here
+
+let valueNumber = Number(score);
+console.log(valueNumber)  //it will give NaN =>which means not a number 
+console.log(typeof(valueNumber));
